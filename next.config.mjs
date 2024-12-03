06fb69434526b1fa49d6f00a7d74a5b57cb448ec
Yuma Noguchi <114992989+Yuma-Noguchi@users.noreply.github.com>
2024-12-03 17:08:05 +0000

    1 featuredeploy (#2)
    * add: add terraform and actions files
    
    * fix: fix workflow files indents
    
    * fix: fix docker file
    
    * fix: fix github actions for building docker

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: "standalone",
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
 };
 
 export default nextConfig;
+
