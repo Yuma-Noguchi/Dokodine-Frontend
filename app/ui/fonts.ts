f1819e69a2ccbdf78217fc0cc39baaead5957f7e
YumaNoguchi39 <n.yuma39@icloud.com>
2024-12-10 14:45:08 +0000

    fix: fix file structures and plugins

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ preload: true, subsets: ['latin'] });
+export const lusitana = Lusitana({ preload: true, subsets: ['latin'], weight: ['400', '700'] });
